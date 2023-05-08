73fb2ecc0217dccee5ebf892b4c51be43a53ea06
Alain SIANI <asiani@free.fr>
2023-05-08 19:57:46 +0200

    Creation des tables

--- OCR/p5/admin.py
+++ OCR/p5/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Comments
 from .models import Users
+from .models import Contributors
+from .models import Projects
+from .models import Issues
+from .models import Comments
 
-admin.site.register(Comments)
 admin.site.register(Users)
+admin.site.register(Contributors)
+admin.site.register(Projects)
+admin.site.register(Issues)
+admin.site.register(Comments)
--- OCR/p5/models.py
+++ OCR/p5/models.py
@@ -15,6 +15,52 @@ class Users(models.Model):
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
+
+class Projects(models.Model):
+    project_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+    author_user_id = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='projects_author_user_id', default=1)
+
+    class Meta:
+        verbose_name_plural = "Projects"
+
+    def __str__(self):
+        return self.title
+
+
+class Contributors(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE, default=1)
+    project_id = models.ForeignKey(Projects, on_delete=models.CASCADE, related_name='contributors_project_id', default=1)   
+    role = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Contributors"
+
+    def __str__(self):
+        return "Contributors"
+
+
+class Issues(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.CharField(max_length=255)
+    tag = models.CharField(max_length=255)
+    priority = models.CharField(max_length=255)
+    project_id = models.ForeignKey(Projects, on_delete=models.CASCADE, related_name='issues_project_id', default=1)
+    author_user_id = models.ForeignKey(Projects, on_delete=models.CASCADE, default=1)
+    status = models.CharField(max_length=255)
+    author_user_id = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='issues_authored_issues', default=1)
+    assignee_user_id = models.ForeignKey(Users, on_delete=models.CASCADE, related_name='issues_assigned_issues', default=1)
+    created_time = models.DateTimeField(auto_now_add=True) 
+
+    class Meta:
+        verbose_name_plural = "Issues"
+
+    def __str__(self):
+        return self.title
+
+
 class Comments(models.Model):
 
     comment_id = models.AutoField(primary_key=True)
